b0fefa5e9c1519d16691013c76d0134b529b8a3c
eduardohernandezguzman <edohdezg@gmail.com>
2025-02-26 10:06:20 +0100

    Añadida validación de formulario de pago

--- payment.js
+++ payment.js
@@ -1,10 +1,25 @@
-const paymentForm = document.getElementById('payment-form');
-const getItem = JSON.parse(localStorage.getItem('CartItem'));
-const totalAmountValue = getItem.reduce((total, item) => total + (item.price * item.quantity), 0);
+const paymentForm = document.getElementById("payment-form");
+const getItem = JSON.parse(localStorage.getItem("CartItem"));
+const totalAmountValue = getItem.reduce(
+  (total, item) => total + item.price * item.quantity,
+  0
+);
 
-paymentForm.addEventListener('submit', (event) => {
-event.preventDefault();
+paymentForm.addEventListener("submit", (event) => {
+  event.preventDefault();
 
-localStorage.removeItem('CartItem');
-window.location.href = 'thankyou.html';
+  const cardNumber = document.querySelector(
+    'input[placeholder="Número de Tarjeta"]'
+  ).value;
+  const expiryDate = document.querySelector(
+    'input[placeholder="Fecha de Expiración (MM/AA)"]'
+  ).value;
+  const cvv = document.querySelector('input[placeholder="CVV"]').value;
+
+  if (!cardNumber || !expiryDate || !cvv) {
+    alert("Por favor, rellena todos los campos del formulario");
+    return;
+  }
+  localStorage.removeItem("CartItem");
+  window.location.href = "thankyou.html";
 });
